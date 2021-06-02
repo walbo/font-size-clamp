602812687d3825a2d7f610e5c332b815354cbf95
Petter Walbø Johnsgård <petter@dekode.no>
2021-06-03 01:12:43 +0200

    Use light theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,26 +8,10 @@ import { useEffect, useState } from 'react';
 /**
  * Internal dependencies
  */
-import { lightTheme, darkTheme } from '@layout/theme';
+import { lightTheme as theme } from '@layout/theme';
 import GlobalStyle from '@layout/global-style';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	const [isDark, setDark] = useState(false);
-	const theme = isDark ? darkTheme : lightTheme;
-
-	useEffect(() => {
-		setDark(
-			window.matchMedia &&
-				window.matchMedia('(prefers-color-scheme: dark)').matches,
-		);
-
-		window
-			.matchMedia('(prefers-color-scheme: dark)')
-			.addEventListener('change', (e) => {
-				setDark(e.matches);
-			});
-	}, []);
-
 	return (
 		<>
 			<ThemeProvider theme={theme}>

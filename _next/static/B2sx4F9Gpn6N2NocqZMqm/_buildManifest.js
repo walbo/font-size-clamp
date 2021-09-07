06086676678bb18df2ca9010c22341ff894e9899
walbo <walbo@users.noreply.github.com>
2021-09-07 20:56:03 +0000

    deploy: ad7300249fd3f8c6339499747252351d80e5a849

--- _next/static/B2sx4F9Gpn6N2NocqZMqm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b47e5da8c1784bafb1da.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

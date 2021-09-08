4a39b66cbf55a7fd4dfb2db7f4843a23a564bebd
Petter Walbø Johnsgård <petter@dekode.no>
2021-09-08 18:32:17 +0200

    Fix TS in key function

--- utils/objects.ts
+++ utils/objects.ts
@@ -1,4 +1,7 @@
-export function hasSameKeys(x: Object, y: Object) {
+export function hasSameKeys(
+	x: Record<string, unknown>,
+	y: Record<string, unknown>,
+): boolean {
 	const xKeys = Object.keys(x);
 	const yKeys = Object.keys(y);
 	return !xKeys.some((k) => !yKeys.includes(k));

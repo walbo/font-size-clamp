425bf7c5c2aa5246a26cd784e432a9d678c0018f
walbo <walbo@users.noreply.github.com>
2021-11-14 19:09:20 +0000

    deploy: 172f34f63e84a497e977c8db91158755cc0d236f

---
+++ _next/static/2q2W5H-2JrLhYevNmk29w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/347-657b4e5575823c42.js","static/chunks/pages/index-a2c0aff9f0b22a8d.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/9IaFojnKeBpCDpWzjK-cc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/347-12eca905be378356.js","static/chunks/pages/index-a2c0aff9f0b22a8d.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

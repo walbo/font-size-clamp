1c19fdb7a139e55ec5022faac34670d3a7b4d3bc
walbo <walbo@users.noreply.github.com>
2022-09-12 19:40:12 +0000

    deploy: e6670416bcd07b1173e074e734aa61f5f27dc66f

---
+++ _next/static/yqGYDqov6SXq0hNzgTH4y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/591-9cd46168f0323cfa.js","static/chunks/pages/index-2769fc5f88f0e10b.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

0a551c222915f5f58fbfa704d32815b9d845011c
walbo <walbo@users.noreply.github.com>
2023-03-27 19:27:38 +0000

    deploy: 180e1c3a895e39de74ceb8e5ca7fbc6416b0bc69

--- _next/static/FvMyIn-cW2tUJ7I753UhR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/523-f37ad0c13365a098.js","static/chunks/pages/index-5f1b4ec23df2a07e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

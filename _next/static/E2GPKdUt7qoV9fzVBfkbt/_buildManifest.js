d2f980f426cf7d348625612adde5da318d57aa36
walbo <walbo@users.noreply.github.com>
2023-04-24 17:56:21 +0000

    deploy: 5d746576636b55360f16550f7489a423ffe46438

---
+++ _next/static/E2GPKdUt7qoV9fzVBfkbt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/523-9765cca32a4104b4.js","static/chunks/pages/index-7b325c645b72c63e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
